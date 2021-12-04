b6db994fd74226aaabf530ecf51eb1146e72088d
Paco <pswift@boomtownroi.com>
2021-12-04 09:30:27 -0800

    parsed sheet data into python class

---
+++ mapping.py
@@ -0,0 +1,15 @@
+# Recipe Fields
+FIELD_ID = 0
+FIELD_NAME = 1
+FIELD_LINK = 2
+FIELD_NOTES = 3
+FIELD_DATE_COOKED = 4
+FIELD_CORE_INGREDIENT = 5
+FIELD_CUISINE = 6
+
+# Google Sheets Setup
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+# The ID and range of a sample spreadsheet.
+RECIPE_SPREADSHEET_ID = '1PsAx6wrv1d1-Ya2SQsOeBvwGAOouIPj0HcpWzh_xNDE'
+RECIPE_COLUMN_RANGE = 'A2:G'
